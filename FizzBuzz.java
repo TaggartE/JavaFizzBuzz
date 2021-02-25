ac21b3dc8095bf611a87383d829e25fcd5a50cae
Taggart Elliott <74575530+TaggartE@users.noreply.github.com>
2021-02-24 18:54:13 -0600

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class FizzBuzz
+{
+    public static void main(String args[])
+    {
+        for(int i=0;i<=100;i++)
+        {
+            String output="";
+
+            if(i%3==0)
+                output+="fizz ";
+            if(i%5==0)
+                output+="buzz";
+            else if(i%5!=0&&i%3!=0)
+                output+=i;
+            System.out.println(output);
+        }
+    }   
+}
